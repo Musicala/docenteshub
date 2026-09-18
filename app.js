503e3d82bf806d3edcf45bd41d642b31263a8f1a
AlekCaballero20 <alekcaballeromusic@gmail.com>
2026-09-18 13:44:42 -0500

    Corrige scroll del chat de Coordinación

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
    - Bitácoras de clase
 */
 
-const BUILD = "2026-09-12.2";
+const BUILD = "2026-09-18.1";
 
 /* Safari iOS puede superponer su barra inferior sobre los elementos fixed.
    VisualViewport entrega el área realmente visible; conservamos la diferencia
